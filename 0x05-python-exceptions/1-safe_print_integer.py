6647e169b11b258ce851520a11f2fc5926faf893
Onyema Justice <onyemajustice@gmail.com>
2022-09-12 07:45:30 -0700

    exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+
     try:
         print("{:d}".format(value))
-    except:
+    except Exception as error:
         return (False)
     else:
         return (True)
